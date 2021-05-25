451bd005e037ad590c5cf18d934b689c41defb9c
Samoht70 <tpoirey70@gmail.com>
2021-05-25 16:05:04 +0200

    Plein de trucs

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public UIButton[] MenuButton;
+    public UIButton OptionsButton;
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MenuButton[0].click)
+        {
+            SceneManager.LoadScene("Maxime");
+        }
+        if (MenuButton[1].click)
+        {
+            mainMenu.SetActive(!mainMenu.activeSelf);
+            optionsMenu.SetActive(!optionsMenu.activeSelf);
+            MenuButton[1].click = false;
+        }
+        if (MenuButton[2].click)
+        {
+            Debug.Log("Vous avez quitté");
+            Application.Quit();
+        }
+
+        if (OptionsButton.click)
+        {
+            mainMenu.SetActive(!mainMenu.activeSelf);
+            optionsMenu.SetActive(!optionsMenu.activeSelf);
+            OptionsButton.click = false;
+        }
+    }
+}
---
+++ Assets/Script/UIButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIButton : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    public bool click;
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        click = true;
+    }
+}
