dadbd7a4185707622106f686f4f61ff06c7875f9
Samoht70 <tpoirey70@gmail.com>
2021-05-26 16:26:43 +0200

    Menu avec anim + options + gestion volume

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -10,37 +10,67 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
-    public PlayableDirector clip;
+    
+    public PlayableDirector clipJouer;
+    public PlayableDirector clipOptions;
+    public PlayableDirector clipRetour;
 
+    public TextMeshProUGUI textVolume;
 
+    public Slider sliderVolume;
 
     // Start is called before the first frame update
     void Start()
     {
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+        if (!PlayerPrefs.HasKey("SoundOptions"))
+        {
+            PlayerPrefs.SetFloat("SoundOptions", 1f);
+        }
+        sliderVolume.value = PlayerPrefs.GetFloat("SoundOptions");
+        textVolume.text = "Volume : " + (int)(sliderVolume.value * 100) + "%";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (sliderVolume.value != PlayerPrefs.GetFloat("SoundOptions"))
+        {
+            PlayerPrefs.SetFloat("SoundOptions", sliderVolume.value);
+            textVolume.text = "Volume : " + (int)(sliderVolume.value * 100) + "%";
+        }
     }
 
     public void Jouer()
     {
         mainMenu.SetActive(!mainMenu.activeSelf);
-        clip.Play();
+        clipJouer.Play();
+        StartCoroutine(Wait3Seconds());
+    }
+    IEnumerator Wait3Seconds()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Maxime");
+    }
+    IEnumerator Wait2Seconds()
+    {
+        yield return new WaitForSeconds(2);
+        
     }
     public void Options()
     {
         mainMenu.SetActive(!mainMenu.activeSelf);
+        clipOptions.Play();
+        StartCoroutine(Wait2Seconds());
         optionsMenu.SetActive(!optionsMenu.activeSelf);
     }
     public void Retour()
     {
-        mainMenu.SetActive(!mainMenu.activeSelf);
         optionsMenu.SetActive(!optionsMenu.activeSelf);
+        clipRetour.Play();
+        StartCoroutine(Wait2Seconds());
+        mainMenu.SetActive(!mainMenu.activeSelf);
     }
     public void Quitter()
     {
---
+++ Assets/Script/MusicLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        audio.volume = PlayerPrefs.GetFloat("SoundOptions");
+    }
+}
