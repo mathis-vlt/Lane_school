8c49a4cb825757c50afb7f5b3acc5b63d0212265
hugo66297 <hugomater@orange.fr>
2021-05-26 10:19:41 +0200

    IA V3

--- Assets/deplacement.cs
+++ Assets/deplacement.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class deplacement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] [Range(0.0F, 100.0F)] private float vitesse;
+    [SerializeField] private int Vdep;
+    [SerializeField] private Camera camera;
+    [SerializeField] private NavMeshAgent prof;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                prof.SetDestination(hit.point);
+            }
+        }
     }
 }
