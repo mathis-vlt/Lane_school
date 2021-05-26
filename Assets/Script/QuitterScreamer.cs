447fff124cb1df8fbf821f8a311a822111820d28
soloPeyret45 <Solo.Peyret@etu.univ-savoie.fr>
2021-05-26 15:16:43 +0200

    Screamer + retout
    Le screamer est terminé + bouton retour à la scene de Thomas

---
+++ Assets/Script/QuitterScreamer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class QuitterScreamer2 : MonoBehaviour
+{
+    public void Exit()
+    {
+        SceneManager.LoadScene("Thomas");
+    }
+}
