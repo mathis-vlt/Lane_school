cd598c67f563d79f2a988614bc65bd69c53fe5c7
Samoht70 <tpoirey70@gmail.com>
2021-05-26 22:48:22 +0200

    Menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -10,10 +10,15 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
+    public GameObject[] button;
+
+    public UIButton bases;
     
     public PlayableDirector clipJouer;
     public PlayableDirector clipOptions;
     public PlayableDirector clipRetour;
+    public PlayableDirector clipBoard;
+    public PlayableDirector clipBoardRet;
 
     public TextMeshProUGUI textVolume;
 
@@ -24,6 +29,8 @@ public class MainMenu : MonoBehaviour
     {
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+        button[1].SetActive(false);
+        button[0].SetActive(true);
         if (!PlayerPrefs.HasKey("SoundOptions"))
         {
             PlayerPrefs.SetFloat("SoundOptions", 1f);
@@ -40,6 +47,19 @@ public class MainMenu : MonoBehaviour
             PlayerPrefs.SetFloat("SoundOptions", sliderVolume.value);
             textVolume.text = "Volume : " + (int)(sliderVolume.value * 100) + "%";
         }
+        if (bases.click)
+        {
+            mainMenu.SetActive(!mainMenu.activeSelf);
+            bases.click = false;
+            button[0].SetActive(!button[0].activeSelf);
+            clipBoard.Play();
+            StartCoroutine(WaitBoard());
+        }
+    }
+    IEnumerator WaitBoard()
+    {
+        yield return new WaitForSeconds(2);
+        button[1].SetActive(!button[1].activeSelf);
     }
 
     public void Jouer()
@@ -72,8 +92,20 @@ public class MainMenu : MonoBehaviour
     }
     IEnumerator WaitRetour()
     {
-        yield return new WaitForSeconds((float)1.7);
+        yield return new WaitForSeconds(2);
         mainMenu.SetActive(!mainMenu.activeSelf);
+    }
+    public void Board()
+    {
+        button[1].SetActive(!button[1].activeSelf);
+        clipBoardRet.Play();
+        StartCoroutine(WaitBoardRet());
+    }
+    IEnumerator WaitBoardRet()
+    {
+        yield return new WaitForSeconds(2);
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        button[0].SetActive(!button[0].activeSelf);
     }
     public void Quitter()
     {
