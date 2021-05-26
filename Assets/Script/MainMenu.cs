7d5639a5da7f38f8d2fd1c0e6a2ee1dd728eeeb1
Samoht70 <tpoirey70@gmail.com>
2021-05-26 02:19:59 +0200

    Encore cinématique menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
-    public Animator transition;
-    public Camera camera;
+    public PlayableDirector clip;
+
 
 
     // Start is called before the first frame update
@@ -28,15 +29,15 @@ public class MainMenu : MonoBehaviour
 
     public void Jouer()
     {
-        
-    }
-    IEnumerator Anim()
-    {
-        transition.SetTrigger("Jouer");
-        yield return new WaitForSeconds(2);
-        transition.SetTrigger("Porte");
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        clip.Play();
     }
     public void Options()
+    {
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
+    }
+    public void Retour()
     {
         mainMenu.SetActive(!mainMenu.activeSelf);
         optionsMenu.SetActive(!optionsMenu.activeSelf);
