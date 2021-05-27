adfa23655e90facd287de51ebdc78645ec6ceca7
Maxime NOZET <nozetm@iut-acy.local>
2021-05-27 17:11:36 +0200

    salle
    salut a tous c la salle.

---
+++ Assets/clignotement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clignotement : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+
+    IEnumerator FlickeringLight()
+    {
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.001f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = true;
+    }
+}
