88a2b087399b1b6cc268b38cf02c274dc7aa71c5
soloPeyret45 <Solo.Peyret@etu.univ-savoie.fr>
2021-05-28 10:15:23 +0200

    menu win
    menu win

---
+++ Assets/Script/menuWin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuWin : MonoBehaviour
+{
+    public void QuitterMenuWin()
+    {
+        SceneManager.LoadScene("Thomas");
+    }
+
+    public void RejouerMenuWin()
+    {
+        SceneManager.LoadScene("Maxime");
+    }
+}
