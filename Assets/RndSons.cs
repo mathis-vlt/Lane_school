7a273a23455a64e2bba0eeee722317735952ef3b
hugo66297 <hugomater@orange.fr>
2021-05-28 10:35:15 +0200

    All Sound
    Ajout de la tracklist de DJ GOD MODE en entière et sons son joué aleatoire

---
+++ Assets/RndSons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RndSons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource laSource;
+    public AudioClip[] listeClip;
+    private int rnd ;
+
+    void Awake()
+    {
+        laSource = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        rnd = Random.Range(0, listeClip.Length);
+        laSource.clip = listeClip[rnd];
+        laSource.PlayOneShot(laSource.clip);
+    }
+    void Update()
+    {
+        if (!laSource.isPlaying)
+        {
+            rnd = Random.Range(0, listeClip.Length);
+            laSource.clip = listeClip[rnd];
+            laSource.PlayOneShot(laSource.clip);
+        }
+    }
+}
